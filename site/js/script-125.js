73fd2c201db39ddc91683b0efff0ffdd90476f90
Joe Gatt <joe@joegatt.net>
2023-06-10 19:39:02 +0200

    Test

--- site/js/script-125.js
+++ site/js/script-125.js
@@ -161,15 +161,11 @@ const initialiseAfterWindow = () => {
 			const audio = Amplitude.getAudio();
 			audio.addEventListener('timeupdate', () => console.log(audio.currentTime) );
 
-// document.getElementById('song-played-progress').addEventListener('click', function( e ){
-//           if( Amplitude.getActiveIndex() == 0 ){
-//             var offset = this.getBoundingClientRect();
-//             var x = e.pageX - offset.left;
-
-//             Amplitude.setSongPlayedPercentage( ( parseFloat( x ) / parseFloat( this.offsetWidth) ) * 100 );
-//           }
-//         });
-
+			document.getElementById('range').addEventListener('click', function(e){
+					var offset = this.getBoundingClientRect();
+					var x = e.pageX - offset.left;
+					Amplitude.setSongPlayedPercentage((parseFloat(x) / parseFloat( this.offsetWidth) ) * 100);
+			});
 		}
 
 
