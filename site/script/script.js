5a97195f511cc026989169ada239c146b47fc728
Joe Gatt <joe@joegatt.net>
2024-10-17 14:26:54 +0300

    Avanti! (auto)

--- site/script/script.js
+++ site/script/script.js
@@ -642,7 +642,7 @@ const initialiseAfterWindow = () => {
 
 
 	document.addEventListener('selectionchange', function (event) {
-		setTimeout(getSelectionText, 10);
+		setTimeout(getSelectionText, 5000);
 	});
 }
 
