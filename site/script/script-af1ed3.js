0d20eb684a7213036d6a0239596adcbf06eab550
Joe Gatt <joe@joegatt.net>
2026-06-01 22:44:58 +0300

    Avanti! (auto)

--- site/script/script-af1ed3.js
+++ site/script/script-af1ed3.js
@@ -11,12 +11,11 @@ const fixReportingTitle = (storyType, sequenceEpisodeNumber, author, pageTitle) 
 	if (storyType === 'Djarju') return `Djarju #${ sequenceEpisodeNumber } ${ author }`;
 	if (!!sequenceEpisodeNumber) return `${ pageTitle } #${ sequenceEpisodeNumber }`;
 	return pageTitle;
-}
+};
 
 const getScrollPosition = () => window.scrollY || document.documentElement.scrollTop;
 
 const getSelectionText = () => {
-  let text = "";
 	if (!!alreadyMarked) return;
 	alreadyMarked = true;
 	var selection = window.getSelection().getRangeAt(0);
@@ -27,11 +26,11 @@ const getSelectionText = () => {
 	if (location.hostname !== 'abbozzi.aphroconfuso.mt') {
 		span.classList.add(window.location.host.split('.')[0]);
 		var credit = document.createElement("div");
-		credit.innerHTML = `<span>“${ pageTitle }”<br><span class="author">${ author }${ !!translator ? "<br>(tr " + translator + ")" : "" }</span></span>`
+		credit.innerHTML = `<span>“${ pageTitle }”<br><span class="author">${ author }${ !!translator ? "<br>(tr " + translator + ")" : "" }</span></span>`;
 		span.appendChild(credit);
 	}
 	selection.insertNode(span);
-}
+};
 
 const newsletterContainer = document.getElementById('newsletter-container-bottom');
 let newsletterPopupShow = true;
@@ -52,7 +51,7 @@ const scrolling = () => {
 		if (!!wordcount) {
 			clearTimeout(hideScrollTools);
 			hideScrollTools = setTimeout(() => {
-				document.body.classList.remove('scrolling')
+				document.body.classList.remove('scrolling');
 			}, 5000);
 			percentageProgress = (((newScrollPosition - bodyStart) * 100) / bodyHeight).toFixed(2);
 			if (percentageProgress >= 0) {
@@ -74,7 +73,7 @@ const scrolling = () => {
 		lastScrollPosition = newScrollPosition;
   }
   return;
-}
+};
 
 const addBookmarkNow = () => {
 	if (!percentageProgress || wordcount < bookmarkThresholdStoryWords || (wordcount * (percentageProgress / 100)) < bookmarkThresholdWords || percentageProgress > 98) {
@@ -98,7 +97,7 @@ const addBookmarkNow = () => {
 		wordcount,
 		wordsPerSecond: wordsPerSecond && wordsPerSecond.toFixed(2),
 	});
-}
+};
 
 const heartbeat = (wordsPerPixel, reportingTitle) => {
 	const timeNow = new Date() / 1000;
@@ -127,7 +126,7 @@ const heartbeat = (wordsPerPixel, reportingTitle) => {
 		// Shall we reset?
 		return;
 	}
-}
+};
 
 // BOOKMARKS *************************************************************************************
 
@@ -184,10 +183,9 @@ const initialiseBookmarksList = () => {
 
 		if (updateFormat || discardBookmark) {
 			delete bookmarksList[oldKey];
-		};
-	});
+		}	});
 	saveBookmarksList();
-}
+};
 
 const addBookmark = (type = 'text', thisStoryId = storyId, bookmark) => {
 	const { author, percentage, sequenceEpisodeNumber, storyType, title } = bookmark;
@@ -201,7 +199,7 @@ const addBookmark = (type = 'text', thisStoryId = storyId, bookmark) => {
 	if (type === 'audio') return;
 	updateBookmarksMenu(bookmarksArray);
 	analytics(['trackEvent', 'Bookmarks', 'żid', reportingTitle, percentage]);
-}
+};
 
 const deleteBookmark = (type = 'text', id = storyId) => {
 	delete bookmarksList[`${ type }-${ id }`];
@@ -216,23 +214,22 @@ const deleteBookmark = (type = 'text', id = storyId) => {
 	removeBookmark.style.opacity = '0';
 	setTimeout(() => removeBookmark.remove(), 1000);
 	analytics(['trackEvent', 'Bookmarks', 'armi', reportingTitle, percentage]);
-}
+};
 
 // FIXME: recalibrate
 const getCurrentBlurb = (percent) => {
 	const currentPlace = Math.round(percent * bodyText.length / 100);
 	const blurb = bodyText.substring(currentPlace, currentPlace + (charactersPerScreen));
 	return blurb;
-}
+};
 
 const updateBookmarksMenu = (bookmarksArray) => {
 	if (!(bookmarksArray && bookmarksMenuElement)) return;
 	if (bookmarksArray.length === 0) {
 		bookmarksMenuElement.textContent = '';
 		return;
-	};
-	bookmarksMenuElement.textContent = ` ${ bookmarksArray.length }`;
-}
+	}	bookmarksMenuElement.textContent = ` ${ bookmarksArray.length }`;
+};
 
 const calculateScrollPosition = (percentage) => Math.round(bodyStart + bodyHeight * (percentage/100));
 
@@ -249,7 +246,7 @@ const showBookmarksInPromos = (bookmarksArray) => {
 			const minutes = numberify(parseInt(remaining / readersWordsPerSecond / 60), ['minuta', 'minuti']);
 			bookmarkLink.innerHTML = `<span class="bookmark-glyph"><!-- --></span> Qrajt ${ roundedPercentage }%, fadallek xi ${ minutes } qari</span>`;
 			bookmarkLink.href = `/${ urlSlug }/#b-${ percentage }`;
-			bookmarkLink.addEventListener("click", () => {analytics(['trackEvent', 'Promo', `minn: ${ reportingTitle }`, `għal: ${ destinationTitle } (bookmark)`, roundedPercentage])});
+			bookmarkLink.addEventListener("click", () => {analytics(['trackEvent', 'Promo', `minn: ${ reportingTitle }`, `għal: ${ destinationTitle } (bookmark)`, roundedPercentage]);});
 			element.appendChild(bookmarkLink);
 		});
 		document.querySelectorAll(`article.story-${ storyId } > header`).forEach((element) => {
@@ -257,17 +254,17 @@ const showBookmarksInPromos = (bookmarksArray) => {
 			const bookmarkContainer = document.createElement("aside");
 			bookmarkContainer.classList.add("bookmark-compact");
 			const bookmarkLink = document.createElement("a");
-			bookmarkLink.innerHTML = `<span class="bookmark-glyph"><!-- --></span> Qrajt ${ roundedPercentage }%, fadallek xi ${ minutes } qari</span>`
+			bookmarkLink.innerHTML = `<span class="bookmark-glyph"><!-- --></span> Qrajt ${ roundedPercentage }%, fadallek xi ${ minutes } qari</span>`;
 			bookmarkLink.href = `#b-${ percentage }`;
 			bookmarkLink.addEventListener('click', () => {
 				analytics(['trackEvent', 'Bookmark fil-paġna', reportingTitle, reportingTitle, roundedPercentage]);
 				window.scrollTo({top: calculateScrollPosition(percentage), left: 0, behavior: 'smooth'});
-			})
+			});
 			bookmarkContainer.appendChild(bookmarkLink);
 			element.appendChild(bookmarkContainer);
 		});
 	});
-}
+};
 
 // REFACTOR: bundle from shared component
 const numberify = (number, words = ['kelma', 'kelmiet']) => {
@@ -282,8 +279,7 @@ const numberify = (number, words = ['kelma', 'kelmiet']) => {
 function prettifyNumbers(text, punctuation = String.fromCharCode(8201)) {
 	if(!text) return null;
 	return text.toString().replace(/\d{1,3}(?=(\d{3})+(?!\d))/g, `$&${ punctuation }`);
-};
-
+}
 const showFullBookmarkList = () => {
 	const list = document.getElementById("bookmark-list");
 	const bookmarksContainer = document.getElementById("bookmarks-container");
@@ -299,7 +295,7 @@ const showFullBookmarkList = () => {
 		bookmarksContainer && bookmarksContainer.classList.add("bookmarks-one");
 	} else if (bookmarksArray.length) {
 		bookmarksContainer && bookmarksContainer.classList.add("bookmarks-multiple");
-		const bookmarksCount = bookmarksArray.length
+		const bookmarksCount = bookmarksArray.length;
 		let numberPhrase = bookmarksCount + ' bookmarks';
 		// use numberify
 		if (bookmarksCount >= 11) numberPhrase = bookmarksCount + '-il bookmark';
@@ -352,13 +348,11 @@ const showFullBookmarkList = () => {
 			document.getElementById(`link-${ storyId }`).addEventListener("click", () => analytics(['trackEvent', 'Promo', 'minn: Bookmarks', `għal: ${ reportingTitle }`, index]));
 		});
 	}
-}
-
-const clearAllBookmarks = () => localStorage.clear();
+};
 
 const getPreviousAudioTime = (id) => {
 	return bookmarksList[`audio-${ id }`] && bookmarksList[`audio-${ id }`].playPosition || 0;
-}
+};
 
 const preventNotesOverlap = () => {
 	const divs = Array.from(document.querySelectorAll('.sequence-12 #podcast-note')).concat(Array.from(document.querySelectorAll('.sequence-12 .fx3')));
@@ -384,13 +378,7 @@ const preventNotesOverlap = () => {
 				div.style.marginTop = '';
 		}
 	}
-}
-
-// INITIALISE ***********************************************************************
-
-const initialiseAfterNewsletter = () => {
-	return;
-}
+};
 
 const initialiseFontSizeListeners = () => {
 	document.getElementById("font-size-1").addEventListener('click', () => addRemoveFontSizeClass(1));
@@ -414,19 +402,19 @@ const initialiseReadingHeartbeat = () => {
 	lastReportedReadingTime = new Date() / 1000;
 	timeStarted = lastReportedReadingTime;
 	setInterval(heartbeat, 3000, wordsPerPixel, reportingTitle);
-}
+};
 
 const initialiseAfterNav = () => {
 	initialiseFontSizeListeners();
 	initialiseThemeSwitch();
-}
+};
 
 const initialiseScrollPosition = () => {
 	if (location.hash && location.hash.startsWith('#b-')) {
 		window.scrollTo({top: calculateScrollPosition(location.hash.substring(3)), left: 0, behavior: 'smooth'});
 		location.hash = '';
 	}
-}
+};
 
 const initialiseMessage = () => {
 	var salted;
@@ -460,7 +448,7 @@ const initialiseMessage = () => {
 		setTimeout(() => document.getElementById('message').classList.remove('active'), 10000);
 		location.hash = '';
 	}
-}
+};
 
 const initialiseNewsletterPopup = () => {
 	const params = !!window.location.search && new URLSearchParams(window.location.search);
@@ -474,20 +462,20 @@ const initialiseNewsletterPopup = () => {
 		newsletterPopupShow = false;
 		setCookie('newsletter', 'popupHide', 0.25);
 	});
-}
+};
 
 const closeNewsletterPopup = () => {
 	newsletterContainer &&newsletterContainer.classList.remove('show', 'popup');
 	analytics(['trackEvent', 'Newsletter', 'popup - inħeba', reportingTitle, percentageProgress]);
 	// analytics(['trackEvent', 'Promo', `minn: ${ reportingTitle }`, `għal: ${ destinationTitle } (bookmark)`, roundedPercentage]);
 	// cookie
-}
+};
 
 const initialiseAnchorEvents = () => {
 	document.querySelectorAll("#grid-body a[href^='\#']").forEach((anchor, index) => {
 		anchor.addEventListener("click", () => analytics(['trackEvent', 'A#', `${ reportingTitle }`, `# ${ anchor.textContent }`, index + 1]));
 	});
-}
+};
 
 document.addEventListener("DOMContentLoaded", async () => {
 	progressElement = document.getElementById('progress');
@@ -535,7 +523,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 				rewind: true,
 				speed: 2000,
 				padding: '2rem 0',
-			}
+			};
 			for (var i = 0; i < slideshows.length; i++) {
 				const newSplide = new Splide(slideshows[i]).mount();
 				newSplide.on('visible', function (slide) {
@@ -547,11 +535,11 @@ document.addEventListener("DOMContentLoaded", async () => {
 		const openLightbox = () => {
 			lightbox.classList.add('open');
 			analytics(['trackEvent', 'Stampi', 'lightbox - iftaħ', reportingTitle]);
-		}
+		};
 		const closeLightbox = () => {
 			lightbox.classList.remove('open');
 			analytics(['trackEvent', 'Stampi', 'lightbox - għalaq', reportingTitle]);
-		}
+		};
 		const lightboxOpen = document.getElementById('lightbox-open');
 		const lightboxClose = document.getElementById('lightbox-close');
 		lightboxOpen && lightboxOpen.addEventListener('click', () => openLightbox());
@@ -569,7 +557,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 			document.body.classList.add('trigger-warning-closed');
 			localStorage.setItem(`tw-${ urlSlug }`, 'magħluq');
 			analytics(['trackEvent', 'Stampi', 'lightbox - għalaq', reportingTitle]);
-		}
+		};
 
 		document.getElementById('trigger-warning-close')?.addEventListener('click', () => closeTriggerWarning());
 
@@ -596,7 +584,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 						analytics(['trackEvent', 'Smiegħ', `Kapitli (${ audioReportingTitle })`, foundLink.textContent]);
 						audio.currentTime = gotoTime;
 						event.preventDefault();
-					})
+					});
 				});
 
 				audio.addEventListener('canplaythrough', () => {
@@ -639,7 +627,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 						translator: song.translator,
 						urlSlug: song.pageSlug,
 					});
-				}
+				};
 
 				const songEvents = (index, song, audio) => {
 					wordsPerSecondAudio = 2.5;
@@ -688,7 +676,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 						previousTime[index] = currentTime;
 					});
 
-				}
+				};
 
 
 			});
@@ -702,8 +690,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 
 			document.querySelectorAll('.audio').forEach(element => element.classList.add('initialised'));
 		}
-	};
-	initialiseMessage();
+	}	initialiseMessage();
 
 	// location.hostname !== 'localhost' &&
 	if (location.hostname !== 'aphroconfuso.mt' && !!storyId) {
@@ -728,7 +715,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 	// Pagefind ********************************************************************************************
 	const searchInput = document.getElementById("search-input");
 	const resultsContainer = document.getElementById("search-results");
-	const pagefind = await import("/pagefind/pagefind.js");
+	const pagefind = await import('../../../../../../pagefind/pagefind.js');
 
 	searchInput && searchInput.addEventListener("input", async () => {
 		const query = searchInput.value.trim();
@@ -773,7 +760,7 @@ document.addEventListener("DOMContentLoaded", async () => {
 	const clearSearch = () => {
 		searchInput.value = "";
 		resultsContainer.innerHTML = "";
-	}
+	};
 
 	document.addEventListener('keydown', function (event) {
 		if (event.key === 'Escape') {
